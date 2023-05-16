4228a33ce224fa061811620b458a98b40805bc3a
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2023-05-16 13:29:30 +0300

    Add empty buffer on init autocmd

--- init.lua
+++ init.lua
@@ -1,18 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 require("configs.options")
 
 require("lazy").setup({ import = "plugins" })
+local base_utils = require("base.autocmd")
+base_utils.empty_buff()
 
 require("base.treesitter")
 require("base.telescope")
